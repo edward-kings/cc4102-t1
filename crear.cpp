71d1f445ac7e6e1c4146270293333a469977337e
nicolas.escobar <tchy258@yahoo.cl>
2023-10-05 02:42:41 -0300

    add: seeds and amount are customizable
    Signed-off-by: nicolas.escobar <tchy258@yahoo.cl>

--- crear.cpp
+++ crear.cpp
@@ -3,11 +3,19 @@
 #include "disk_manipulation_functions/generateRectangles.hpp"
 
 
-int main() {
+int main(int argc, char** argv) {
+    int seed1 = 3;
+    int seed2 = 4;
+    int amount = 1 << 10;
+    if (argc >= 3) {
+        seed1 = atoi(argv[1]);
+        seed2 = atoi(argv[2]);
+        amount = argc == 4 ? atoi(argv[3]) : amount;
+    }
     std::fstream rectangulos;
-    generateRectanglesFile(rectangulos,"rectangulos.bin",true, 1 << 10,3,false);
+    generateRectanglesFile(rectangulos,"rectangulos.bin",true, amount,seed1,false);
     std::fstream querys;
-    generateRectanglesFile(querys,"querys.bin",true, 1 << 10, 2, true);
+    generateRectanglesFile(querys,"querys.bin",true, 100, seed2, true);
     rectangulos.close();
     querys.close();
     return 0;
