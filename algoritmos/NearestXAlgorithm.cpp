894c30acd0af792bc3f81cc174cec185d0f77aca
nicolas.escobar <tchy258@yahoo.cl>
2023-10-17 03:53:39 -0300

    refactor: move tree build logic to interface, setter methods
    Signed-off-by: nicolas.escobar <tchy258@yahoo.cl>

--- algoritmos/NearestXAlgorithm.cpp
+++ algoritmos/NearestXAlgorithm.cpp
@@ -19,18 +19,42 @@ NearestXAlgorithm::NearestXAlgorithm(unsigned int maxNodeCapacity, unsigned int 
   this->numberOfRects = numberOfRects;
 }
 
+
+unsigned int NearestXAlgorithm::getNumberOfRects() {
+  return this->numberOfRects;
+}
+
+unsigned int NearestXAlgorithm::getMaxNodeCapacity() {
+  return this->maxNodeCapacity;
+}
+
+/**
+ * @brief Getter para el nombre del algoritmo
+*/
+std::string NearestXAlgorithm::getAlgorithmName() {
+  return "NearestX";
+}
+
 /**
  * @brief Getter del archivo que contiene los nodos internos del RTree.
 */
-std::istream& NearestXAlgorithm::getTreeFile() {
+std::istream* NearestXAlgorithm::getTreeFile() {
   return this->treeFile;
 }
 
 /**
  * @brief Getter del archivo que contiene las hojas del RTree.
 */
-std::istream& NearestXAlgorithm::getLeavesFile() {
+std::istream* NearestXAlgorithm::getLeavesFile() {
   return this->leavesFile;
+}
+
+void NearestXAlgorithm::setTreeFile(std::fstream* fileStream) {
+  this->treeFile = fileStream;
+}
+
+void NearestXAlgorithm::setLeavesFile(std::fstream* fileStream) {
+  this->leavesFile = fileStream;
 }
 
 /**
@@ -43,73 +67,8 @@ bool NearestXAlgorithm::orderCriteria(const Rect& rect1, const Rect& rect2) {
   return rect1Center < rect2Center;
 }
 
-/**
- * @brief Método que construye un RTree utilizando el algoritmo NearestX.
- * \param filename Nombre del archivo de rectángulos.
-*/
-void NearestXAlgorithm::buildTree(std::string filename) {
-  std::fstream rectFile(filename, std::ios::in | std::ios::binary);
-  // Se lee el archivo de rectángulos y se ordenan por su centro en el eje X
-  unsigned int bufferSize = this->numberOfRects;
-  Rect* rectBuffer = new Rect[bufferSize];
-  rectFile.read(reinterpret_cast<char*>(rectBuffer), sizeof(Rect) * bufferSize);
-  rectFile.close();
+
+void NearestXAlgorithm::preProcess(Rect* rects, unsigned int arraySize) {
   auto orderLambda = [this](const Rect& rect1, const Rect& rect2) { return this->orderCriteria(rect1, rect2); };
-  std::sort(rectBuffer, rectBuffer + bufferSize, orderLambda);
-  std::fstream& sortedRects = this->leavesFile;
-  sortedRects = std::fstream("sortedRects.bin", std::ios::out | std::ios::in | std::ios::binary | std::ios::trunc);
-  sortedRects.write(reinterpret_cast<char*>(rectBuffer), sizeof(Rect) * bufferSize);
-  // amountOfNodes = n/M: cantidad de nodos internos
-  unsigned int amountOfNodes = std::ceil((double)numberOfRects / this->maxNodeCapacity);
-  unsigned int nodeNumber = 0;
-  unsigned int currentRect = 0;
-  // Indica la altura actual del árbol
-  unsigned int currentLevel = 0;
-  std::vector<std::vector<RTreeNode>> nodeBuffer;
-  nodeBuffer.push_back(std::vector<RTreeNode>(amountOfNodes));
-  while (nodeNumber < amountOfNodes) {
-    RTreeNode node;
-    node.MBR.x1 = rectBuffer[currentRect].x1;
-    node.MBR.y1 = rectBuffer[currentRect].y1;
-    // Las hojas tienen índices negativos para diferenciarlos de los nodos internos
-    node.firstChildIndex = -(currentRect * sizeof(Rect));
-    currentRect = std::min(currentRect + this->maxNodeCapacity - 1, this->numberOfRects - 1);
-    node.MBR.x2 = rectBuffer[currentRect].x2;
-    node.MBR.y2 = rectBuffer[currentRect].y2;
-    node.lastChildIndex = -(currentRect * sizeof(Rect));
-    nodeBuffer[currentLevel][nodeNumber] = node;
-    currentRect++;
-    nodeNumber++;
-  }
-  currentLevel++;
-  unsigned int currentNodeIndex = 0;
-  // Mientras queden nodos por crear
-  while (amountOfNodes > 1) {
-    amountOfNodes = std::ceil((double)amountOfNodes / this->maxNodeCapacity);
-    nodeBuffer.push_back(std::vector<RTreeNode>(amountOfNodes));
-    for (unsigned int i = 0; i < amountOfNodes; i++) {
-      RTreeNode node;
-      node.MBR.x1 = nodeBuffer[currentLevel - 1][currentNodeIndex].MBR.x1;
-      node.MBR.y1 = nodeBuffer[currentLevel - 1][currentNodeIndex].MBR.y1;
-      node.firstChildIndex = (currentNodeIndex + 1) * sizeof(RTreeNode) * currentLevel;
-      currentNodeIndex = std::min((unsigned long long)(currentNodeIndex + this->maxNodeCapacity - 1), (unsigned long long)(nodeBuffer[currentLevel - 1].size() - 1));
-      node.MBR.x2 = nodeBuffer[currentLevel - 1][currentNodeIndex].MBR.x2;
-      node.MBR.y2 = nodeBuffer[currentLevel - 1][currentNodeIndex].MBR.y2;
-      node.lastChildIndex = (currentNodeIndex + 1) * sizeof(RTreeNode) * currentLevel;
-      nodeBuffer[currentLevel][i] = node;
-    }
-    currentLevel++;
-    currentNodeIndex = 0;
-  }
-  delete[] rectBuffer;
-  RTreeNode* nodeBufferArray = new RTreeNode[(int)(std::ceil((double)numberOfRects / this->maxNodeCapacity))];
-  std::fstream& nearestXFile = this->treeFile;
-  nearestXFile = std::fstream("NearestXRTree.bin", std::ios::out | std::ios::in | std::ios::binary | std::ios::trunc);
-  for (int i = nodeBuffer.size() - 1; i >= 0; i--) {
-    for (unsigned int j = 0; j < nodeBuffer[i].size(); j++) {
-      nodeBufferArray[j] = nodeBuffer[i][j];
-    }
-    nearestXFile.write(reinterpret_cast<char*>(nodeBufferArray), sizeof(RTreeNode) * nodeBuffer[i].size());
-  }
-  delete[] nodeBufferArray;
+  std::stable_sort(rects, rects + arraySize, orderLambda);
 }
--- algoritmos/NearestXAlgorithm.hpp
+++ algoritmos/NearestXAlgorithm.hpp
@@ -19,19 +19,26 @@ public:
    * Cierra los archivos treeFile y leavesFile.
   */
   ~NearestXAlgorithm() {
-    this->treeFile.close();
-    this->leavesFile.close();
+    this->treeFile->close();
+    this->leavesFile->close();
+    delete this->treeFile;
+    delete this->leavesFile;
   }
   NearestXAlgorithm(unsigned int maxNodeCapacity, unsigned int numberOfRects);
-  void buildTree(std::string filename) override;
-  std::istream& getTreeFile() override;
-  std::istream& getLeavesFile() override;
+  std::string getAlgorithmName() override;
+  std::istream* getTreeFile() override;
+  std::istream* getLeavesFile() override;
+  unsigned int getNumberOfRects() override;
+  unsigned int getMaxNodeCapacity() override;
 private:
-  bool orderCriteria(const Rect& rect1, const Rect& rect2);
-  std::fstream treeFile;
-  std::fstream leavesFile;
+  bool orderCriteria(const Rect& rect1, const Rect& rect2) override;
+  std::fstream* treeFile;
+  std::fstream* leavesFile;
+  void setTreeFile(std::fstream* fileStream) override;
+  void setLeavesFile(std::fstream* fileStream) override;
   unsigned int maxNodeCapacity;
   unsigned int numberOfRects;
+  void preProcess(Rect* rects, unsigned int arraySize) override;
 };
 
 #endif // NEAREST_X_ALGORITHM_H
